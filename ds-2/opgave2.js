f21c783f04a2ecac61163919bb11a7342612b1c5
Dalena <79627422+dalenatrinh@users.noreply.github.com>
2023-01-26 13:05:35 +0100

    Nye mappe med ds-1 og tilføjelse af kode i opgave 2

--- ds-2/opgave2.js
+++ ds-2/opgave2.js
@@ -6,27 +6,27 @@ let output = document.getElementById("output");
 *   Et program der genere et one time pad: random tal og numre ud fra input.
 *   1. Genere random nummer og tal.
 *   2. Det gereneret nummer skal være lige så lang som input.
-*
+*   3. Tilføjer selve input og ekstra random numre/bogstaver.
 *  */
-
-
-// Fundet på stackoverflow for at genere random tal og bogstaver:
-function makeid(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-    }
-    return result;
-}
 
 // Læser fra input:
 btn.addEventListener("click", function() {
     let inputValue = input.value;
     console.log(inputValue);
-    console.log(makeid(inputValue.length));
+    output.innerHTML = makeid(inputValue.length);
+
+    // Funktion i en funktion for der er variabler der skal bruges, det virker ikke globalt (inputValue).
+    // Fundet på stackoverflow for at genere random tal og bogstaver:
+    function makeid(length) {
+        let result = inputValue + '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
+        }
+        return result;
+    }
 })
 
